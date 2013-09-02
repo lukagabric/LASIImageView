7ef4665fe5893a17937207f6befc64f1005ef8be
Luka Gabric <luka.gabric@gmail.com>
2013-09-02 12:56:35 +0200

    layout

--- LASIImageView/Classes/ViewControler/ViewController.m
+++ LASIImageView/Classes/ViewControler/ViewController.m
@@ -8,11 +8,7 @@
 {
     [super viewDidLoad];
     
-    LProgressAppearance *appearance = [LProgressAppearance new];
-    appearance.showPercentageInAnnular = NO;
-    _imageView.progressViewAppearance = appearance;
-    
-    _imageView.imageUrl = @"http://images.sodahead.com/slideshows/000003527/polls_toadily_insane_4725_290286_answer_1_xlarge-31143707557_xlarge.jpeg";
+    _imageView.imageUrl = @"http://farm9.staticflickr.com/8190/8148007408_8fbac75988_o.jpg";
 }
 
 
