5613415bd3ea36816696fa0158396a461a486709
Luka Gabric <luka.gabric@gmail.com>
2013-09-04 11:04:39 +0200

    appearance

--- LASIImageView/Classes/ViewControler/ViewController.m
+++ LASIImageView/Classes/ViewControler/ViewController.m
@@ -8,8 +8,8 @@
 {
     [super viewDidLoad];
     
-    [[LProgressView progressAppearance] setType:LProgressTypeCircle];
-    [[LProgressView progressAppearance] setSchemeColor:[UIColor redColor]];
+    [[LProgressView progressAppearance] setType:LProgressTypePie];
+    [[LProgressView progressAppearance] setSchemeColor:[UIColor whiteColor]];
     
     _imageView.imageUrl = @"http://farm9.staticflickr.com/8190/8148007408_8fbac75988_o.jpg";
 }
